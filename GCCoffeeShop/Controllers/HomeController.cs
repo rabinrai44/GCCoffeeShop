0e72641de4ac24a9907d75b4c93b6900c2135c87
Rabin Rai <rabinrai44@gmail.com>
2018-08-13 15:08:45 -0400

    Added Register model, register form, welcome screen with hello firstname, controller register HttpGet & HttpPost to getting value from user enter

--- GCCoffeeShop/Controllers/HomeController.cs
+++ GCCoffeeShop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GCCoffeeShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,24 @@ namespace GCCoffeeShop.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register user)
+        {
+            return RedirectToAction("Welcome", user);
+
+        }
+
+        public ActionResult Welcome(Register user)
+        {
+            return View(user);
         }
     }
 }
---
+++ GCCoffeeShop/Models/Register.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCCoffeeShop.Models
+{
+    public class Register
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+    }
+}
