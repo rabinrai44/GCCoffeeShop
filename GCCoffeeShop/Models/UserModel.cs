18afe9682869151160d32408ed18c04151882d8a
Rabin Rai <rabinrai44@gmail.com>
2018-08-14 09:03:39 -0400

    Added form validation with jQueryval

--- GCCoffeeShop/Models/UserModel.cs
+++ GCCoffeeShop/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,27 @@ namespace GCCoffeeShop.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage = "First Name Required")]
+        [Display(Name = "First Name")]
+        [StringLength(30, MinimumLength = 3)]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(30, MinimumLength = 3)]
         public string LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}")]
         public DateTime DOB { get; set; }
+
+        [Required(ErrorMessage = "Email Required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone Number")]
         public string Phone { get; set; }
+
         public string Country { get; set; }
     }
 }
